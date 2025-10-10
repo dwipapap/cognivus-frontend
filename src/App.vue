6c4243dda6a44af72964aee3ffd1f908f56f262a
dwipapap <dwipahyaki@gmail.com>
2025-10-10 17:48:16 +0700

    changing so that navbar is only in the home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-
-const noNavbarRoutes = ['Login', 'StudentDashboard', 'StudentProfile', 'StudentProfileView', 'LecturerDashboard', 'LecturerProfileView'];
 </script>
 
 <template>
   <div>
-    <Navbar v-if="!noNavbarRoutes.includes($route.name)" />
     <main>
       <router-view />
     </main>
