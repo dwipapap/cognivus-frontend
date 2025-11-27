d8686ddcaa5b63b61a08c0d8fafddab062efaf87
dwipapap <dwipahyaki@gmail.com>
2025-11-27 15:32:30 +0700

    nambah icon dari iconify biar cantik

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-    tailwindcss()
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true
+    })
   ],
   resolve: {
     alias: {
