180d63a9fd907e6b2005189ad3c8a9abcf5aff7e
dwipapap <dwipahyaki@gmail.com>
2026-06-01 16:38:46 +0700

    fix(login): align BaseButton usage and modal for Nuxt UI
    Use label prop on BaseButton instead of slot content for primary
    button. Change Google login variant to 'light'. Add alert prop
    to Modal for notification-style dialog. Wire @confirm event.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -202,9 +202,8 @@ const handleGoogleLogin = () => {
               size="lg"
               block
               :loading="isSubmitting"
-            >
-              Login
-            </BaseButton>
+              label="Login"
+            />
           </form>
 
           <!-- Divider -->
@@ -217,9 +216,10 @@ const handleGoogleLogin = () => {
           <!-- Google Login Button -->
           <BaseButton
             @click="handleGoogleLogin"
-            variant="secondary"
+            variant="light"
             size="lg"
             block
+            label="Login with Google"
           >
             <template #icon>
               <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -229,7 +229,6 @@ const handleGoogleLogin = () => {
                 <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
               </svg>
             </template>
-            Login with Google
           </BaseButton>
 
           <!-- Footer text -->
@@ -252,11 +251,13 @@ const handleGoogleLogin = () => {
 
     <!-- Modal Component -->
     <Modal
+      alert
       :show="showModal"
       :type="modalType"
       :title="modalTitle"
       :message="modalMessage"
       @close="closeModal"
+      @confirm="closeModal"
     />
 
     <!-- Forgot Password OTP Flow -->
